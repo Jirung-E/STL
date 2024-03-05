4e87fea2a97ad05c6ec218c3d52b25708c6e8374
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-05 17:29:58 +0900

    print max value

--- stl.cpp
+++ stl.cpp
@@ -1,21 +1,48 @@
 // -------------------------------------------------------------------------------
-// 2024 STL 3.5 월910                                                       (1주1)
+// 2024 1학기 STL 월910화78     3월 5일 화요일                                (1주2)
 // 
-// Visual Studio 17.9.0 이상, Release/x64
-// Project 속성 / std::c++latest, SDL 검사 - no
-// 
-// 한학기 강의를 저장하는 save 만들기
+// 인터넷에서 C++언어 관련 설명을 찾아본다
 // -------------------------------------------------------------------------------
 
 #include <iostream>
+#include <random>
+#include <format>
 
 #include "save.h"
 
 using namespace std;
 
 
+//random_device rd;
+default_random_engine dre;		// 크기가 커서 전역변수로 한다
+uniform_int_distribution uid { 1000, 9999 };
+
+
+// [문제] 가장 큰 수를 찾아 화면에 출력한다.
+
+
 int main() {
-	cout << "STL 입니다." << '\n';
+	int a[1000];
+
+	for(int& num : a) {
+		num = uid(dre);
+		cout << format("{:8}", num);
+	}
+
+
+
+	int max = numeric_limits<int>::min();
+
+	for(int num : a) {
+		if(max < num) {
+			max = num;
+		}
+	}
+
+	cout << "최댓값 - " << max << endl;
+
+	cout << *std::max_element(a, a+1000) << endl;
+	cout << *std::max_element(begin(a), end(a)) << endl;
 
 	save("stl.cpp");
 }
