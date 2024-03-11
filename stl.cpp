072bae0989f5ab5a926ce615dc39f80e3b35d998
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-11 18:57:45 +0900

    array의 구현?

--- stl.cpp
+++ stl.cpp
@@ -3,47 +3,30 @@
 // 
 // -------------------------------------------------------------------------------
 
-#include <iostream>
-#include <fstream>
-#include <random>
-#include <format>
-#include <algorithm>
 #include <print>
-#include <array>
-#include <list>
 
 #include "save.h"
 
 using namespace std;
 
 
-//random_device rd;
-default_random_engine dre;		// 크기가 커서 전역변수로 한다
-uniform_int_distribution uid { 0, 99'999 };
+template<typename T, size_t size>
+class Array {
+	T arr[size];
 
-
-// [문제] 파일 "int값들.txt"에는 몇 개인지 모르는 int가 저장되어있다. 
-// 모두 몇 개인지 출력하고 그 중에 가장 큰 값도 화면에 출력하라.
-
+public:
+	T& operator[](size_t index) {
+		return arr[index];
+	}
+};
 
 int main() {
-	ifstream in { "int값들.txt" };
-	if(not in) {
-		exit(0);
-	}
+	Array<int, 100> arr;
 
-	int num;
-	int cnt {};
-	int max_value = numeric_limits<int>::min();
-	while(in >> num) {
-		++cnt;
-		if(max_value < num) {
-			max_value = num;
-		}
-	}
-
-	println("cnt: {}", cnt);
-	println("max: {}", max_value);
+	println("Array<int, 100> size: {}", sizeof(arr));
+	println("arr[10]: {}", arr[0]);
 
 	save("stl.cpp");
 }
+
+
