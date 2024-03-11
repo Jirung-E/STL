91c4a28e4a65e449eac25832f1fab28b8b68666e
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-11 17:59:21 +0900

    array

--- stl.cpp
+++ stl.cpp
@@ -1,12 +1,13 @@
 // -------------------------------------------------------------------------------
-// 2024 1학기 STL 월910화78     3월 5일 화요일                                (1주2)
+// 2024 1학기 STL 월910화78     3월 11일 월요일                              (2주1)
 // 
-// 인터넷에서 C++언어 관련 설명을 찾아본다
 // -------------------------------------------------------------------------------
 
 #include <iostream>
 #include <random>
 #include <format>
+#include <print>
+#include <algorithm>
 
 #include "save.h"
 
@@ -22,27 +23,16 @@ uniform_int_distribution uid { 1000, 9999 };
 
 
 int main() {
-	int a[1000];
+    int a[1000];	// contiguous
 
 	for(int& num : a) {
 		num = uid(dre);
-		cout << format("{:8}", num);
+		print("{:8}", num);
 	}
 
+	cout << endl;
 
-
-	int max = numeric_limits<int>::min();
-
-	for(int num : a) {
-		if(max < num) {
-			max = num;
-		}
-	}
-
-	cout << "최댓값 - " << max << endl;
-
-	cout << *std::max_element(a, a+1000) << endl;
-	cout << *std::max_element(begin(a), end(a)) << endl;
+	print("최댓값 - {}", *max_element(begin(a), end(a)));
 
 	save("stl.cpp");
 }
