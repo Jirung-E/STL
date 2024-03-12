41c453cbe17db121ef9da1bc174eb88fcfc1afea
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-12 17:52:03 +0900

    문제 완료

--- stl.cpp
+++ stl.cpp
@@ -4,6 +4,7 @@
 // --------------------------------------------------------------------------------
 
 #include <iostream>
+#include <fstream>
 #include <print>
 #include <random>
 #include <array>
@@ -23,6 +24,10 @@ class Dog {
     int num { uidNum(dre) };
 
 public:
+    bool operator<(const Dog& dog) const {
+        return num < dog.num;
+    }
+
     friend ostream& operator<<(ostream& os, const Dog& dog) {
         return os << "글자: " << dog.c << ", 숫자: " << dog.num;
     }
@@ -30,14 +35,16 @@ public:
 
 
 int main() {
-    array<Dog, 100> dogs;
-
-    for(const Dog& dog : dogs) {
-        cout << dog << endl;
-    }
-
     // [문제] dogs를 binary mod/write로 파일에 기록하라.
+    //ofstream out { "dogs", ios::binary };
+    //out.write((const char*)dogs.data(), sizeof(dogs));
+    
     // 기록된 파일을 읽어서 num값이 가장 큰 Dog를 찾아 화면에 출력하라.
+    array<Dog, 100> dogs;
+    ifstream in { "dogs", ios::binary };
+    in.read((char*)dogs.data(), sizeof(dogs));
+
+    cout << *max_element(dogs.begin(), dogs.end()) << endl;
 
     save("stl.cpp");
 }
