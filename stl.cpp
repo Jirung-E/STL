c52699ef1d126f9affc3aa0e3b6d314a367d151f
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-18 18:58:28 +0900

    3-1

--- stl.cpp
+++ stl.cpp
@@ -5,48 +5,13 @@
 // --------------------------------------------------------------------------------
 
 #include <iostream>
-#include <fstream>
-#include <array>
-#include <algorithm>
 
 #include "save.h"
 
 using namespace std;
 
-// [문제] "dogs" 파일은 binary모드로 기록하였다. 
-// 여기에는 class Dog객체 100개를 write함수를 사용하여 기록하였다.
-// class Dog의 멤버는 다음과 같다.
-// 파일을 읽어 num값이 홀수인 Dog객체의 수를 출력하라.
-
-class Dog {
-    char c;
-    int num;
-
-public:
-    int getNum() const {
-        return num;
-    }
-
-    void show() const {
-        cout << "Dog: { " << c << ", " << num << " }" << endl;
-    }
-};
-
 
 int main() {
-    ifstream in { "dogs", ios::binary };
-    if(not in) {
-        cout << "파일을 열 수 없다." << endl;
-        return 0;
-    }
-
-    array<Dog, 100> dogs;
-    in.read((char*)dogs.data(), sizeof(dogs));
-
-    int count = count_if(dogs.begin(), dogs.end(), [](const Dog& dog) {
-        return dog.getNum() % 2;
-    });
-    cout << count << endl;
 
     save("stl.cpp");
 }
