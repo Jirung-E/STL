58f3baa8887350d748b5a2792d2f638a41c61958
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-19 16:10:15 +0900

    메모리 구조

--- stl.cpp
+++ stl.cpp
@@ -1,17 +1,29 @@
 // --------------------------------------------------------------------------------
-// 2024 1학기 STL  월910화78        3월 18일 월요일                            (3주1)
+// 2024 1학기 STL  월910화78        3월 19일 화요일                            (3주2)
 // 
 // 실행 파일의 메모리 영역 - STACK, CODE, DATA, Free store(heap)
 // --------------------------------------------------------------------------------
 
 #include <iostream>
+#include <string>
 
 #include "save.h"
 
 using namespace std;
 
 
-int main() {
+void f() {
+    int num { 123 };
+}
 
-    save("stl.cpp");
+int gnum {};
+
+int main() {
+    string s { "메모리 관찰" };
+
+    f();
+
+    string* p = new string[2];
+
+    //save("stl.cpp");
 }
