5d2b8af61798611398aa4da7e849bac35571de17
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-04-22 18:33:23 +0900

    list::sort

--- stl.cpp
+++ stl.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <algorithm>
 
 #include "String.h"
 #include "save.h"
@@ -22,8 +23,6 @@ extern bool 관찰;
 int main() {
     list<String> cont;
 
-    관찰 = true;
-
     // [문제] "String.cpp"에 있는 단어를 cont에 저장하라.
     ifstream ifs { "String.cpp" };
     if(not ifs) {
@@ -33,11 +32,21 @@ int main() {
 
     cont = { istream_iterator<String>{ifs}, {} };
 
+    관찰 = true;
+
+    // [문제] 길이 오름차순으로 정렬
+    cont.sort([](const String& s1, const String& s2) {
+        return s1.getLen() < s2.getLen();
+    });
+    //sort(cont.begin(), cont.end(), [](const String& s1, const String& s2) {
+    //    return s1.getLen() < s2.getLen();
+    //});
+
     관찰 = false;
 
     for(const auto& e : cont) {
         cout << e << endl;
     }
 
-    save("stl.cpp");
+    //save("stl.cpp");
 }
