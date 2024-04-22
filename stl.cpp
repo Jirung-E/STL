0e841334dd9454ae1d12abea2d041fc677f9db31
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-04-22 18:05:17 +0900

    lis

--- stl.cpp
+++ stl.cpp
@@ -1,15 +1,14 @@
 // --------------------------------------------------------------------------------
-// 2024 1학기 STL  월910화78        4월 16일 화요일                            (7주2)
+// 2024 1학기 STL  월910화78        4월 22일 월요일                            (8주1)
 // 
 // 5월 6일 월요일 강의 함
 // 4월 23일 시험
 // 
-// deque - vector와 list의 중간형태를 띄는 자료구조(stl에서)
+// list
 // --------------------------------------------------------------------------------
 
 #include <iostream>
-#include <deque>
-#include <vector>
+#include <fstream>
 #include <list>
 
 #include "String.h"
@@ -20,26 +19,27 @@ using namespace std;
 extern bool 관찰;
 
 
-class Test {
-    char x[4096];
-};
-
-
 int main() {
-    //vector<Test> v;     // 2362204
-    deque<Test> v;        // 46~~~~~
-    //list<Test> v;       // ??
+    list<String> cont;
 
-    while(true) {
-        try {
-            v.push_back(Test {});
-        }
-        catch(...) {
-            cout << endl << endl;
-            cout << "여기까지" << endl;
-            cout << v.size() << endl;
-            break;
-        }
+    관찰 = true;
+
+    // [문제] "String.cpp"에 있는 단어를 cont에 저장하라.
+    ifstream ifs { "String.cpp" };
+    if(not ifs) {
+        cout << "파일을 열지 못했습니다." << endl;
+        return 0;
+    }
+
+    String s;
+    while(ifs >> s) {
+        cont.emplace_back(s);       // 이렇게 하면 push_back과 똑같이 동작
+    }
+
+    관찰 = false;
+
+    for(const auto& e : cont) {
+        cout << e << endl;
     }
 
     //save("stl.cpp");
