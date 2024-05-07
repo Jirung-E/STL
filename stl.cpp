c92fe12282762a1a5165d1ecbc3eab34c897b41e
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-07 17:22:07 +0900

    이걸 해결해라!

--- stl.cpp
+++ stl.cpp
@@ -5,6 +5,7 @@
 // --------------------------------------------------------------------------------
 
 #include <iostream>
+#include <algorithm>
 
 #include "String.h"
 #include "save.h"
@@ -25,21 +26,9 @@ void f(Iter iter) {
 int main() {
     String s { "1357924680" };
 
-    // 반복자의 속성을 확인하는 표준 질문에 응답할 수 있어야 한다.
-    // C++ 표준을 지키는 반복자가 되려면 다음 다섯가지 항목을 정의해야 한다.
-    // difference_type	   
-    // value_type	       
-    // pointer	           
-    // reference	       
-    // iterator_category
+    sort(s.rbegin(), s.rend());
 
-    f(s.begin());
-    f(s.rbegin());
-
-    for(auto it=s.rbegin(); it!=s.rend(); ++it) {
-        cout << *it << ' ';
-    }
-    cout << endl;
+    cout << s << endl;
 
     //save("stl.cpp");
 }
