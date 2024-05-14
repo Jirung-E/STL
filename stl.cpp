20b0bf280c677463bc247b29d86e56371fab9083
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-14 16:44:25 +0900

    my_copy

--- stl.cpp
+++ stl.cpp
@@ -15,67 +15,18 @@ using namespace std;
 extern bool 관찰;
 
 
- //세번째 인자는 callable type인데 이 중에서 boolean값을 리턴하는 것을 
- //특별히 predicate이라고 부른다(조건자, 서술자, 판단자)
-template <class Iter, class Cond>
-Iter my_find_if(Iter first, Iter last, Cond func) {
-    while(last != first) {
-        if(func(*first)) {
-            return first;
-        }
-        ++first;
+template <class SIter, class DIter>
+void my_copy(SIter first, SIter end, DIter dest) {
+    for(SIter it=first; it<end; ++it) {
+        *dest = *it;
+        ++dest;
     }
-    return last;
 }
-
 
 int main() {
     //save("stl.cpp");
 
     String s { "20240514 기말시험 6월 15일 15주 2일" };
 
-    // [문제] 사용자가 찾는 조건에 맞는 글자가 s에 있는지 알려주자
-    // 조건: 숫자를 찾아서 홀수인지
-
-    auto p = s.begin();
-    while(p != s.end()) {
-        p = my_find_if(p+1, s.end(), [](char c) {
-            if(isdigit(c)) {
-                if(c & 1) {
-                    return true;
-                }
-            }
-            return false;
-        });
-
-        if(p != s.end()) {
-            cout << *p << "는 s의 " << distance(s.begin(), p) + 1
-                << "번째 문자입니다." << endl;
-        }
-        else {
-            break;
-        }
-    }
-
-    auto beg = s.begin();
-    while(true) {
-        auto p = my_find_if(beg, s.end(), [](char c) {
-            if(isdigit(c)) {
-                if(c & 1) {
-                    return true;
-                }
-            }
-            return false;
-        });
-
-        if(p != s.end()) {
-            cout << *p << "는 s의 " << distance(s.begin(), p) + 1
-                << "번째 문자입니다." << endl;
-        }
-        else {
-            break;
-        }
-
-        beg = p + 1;
-    }
+    my_copy(s.begin(), s.end(), ostream_iterator<char>{cout});
 }
