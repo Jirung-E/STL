d252933850d3c2438f98f6b4ec097d90e6b42529
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-14 16:33:07 +0900

    모든 홀수 찾기

--- stl.cpp
+++ stl.cpp
@@ -6,7 +6,6 @@
 // --------------------------------------------------------------------------------
 
 #include <iostream>
-#include <functional>
 
 #include "String.h"
 #include "save.h"
@@ -16,25 +15,15 @@ using namespace std;
 extern bool 관찰;
 
 
-// 세번째 인자는 callable type인데 이 중에서 boolean값을 리턴하는 것을 
-// 특별히 predicate이라고 부른다(조건자, 서술자, 판단자)
-//template <class Iter, class Cond>
-//Iter my_find_if(Iter first, Iter last, Cond func) {
-//    while(last != first) {
-//        if(func(*first)) {
-//            return first;
-//        }
-//        ++first;
-//    }
-//    return last;
-//}
-
-template <class Iter>
-Iter my_find_if(Iter first, Iter last, function<bool(decltype(*first))> func) {
-    for(Iter it=first; it<last; ++it) {
-        if(func(*it)) {
-            return it;
+ //세번째 인자는 callable type인데 이 중에서 boolean값을 리턴하는 것을 
+ //특별히 predicate이라고 부른다(조건자, 서술자, 판단자)
+template <class Iter, class Cond>
+Iter my_find_if(Iter first, Iter last, Cond func) {
+    while(last != first) {
+        if(func(*first)) {
+            return first;
         }
+        ++first;
     }
     return last;
 }
@@ -43,18 +32,50 @@ Iter my_find_if(Iter first, Iter last, function<bool(decltype(*first))> func) {
 int main() {
     //save("stl.cpp");
 
-    String s { "stl algorithm - 2024년 5월 14일" };
+    String s { "20240514 기말시험 6월 15일 15주 2일" };
 
     // [문제] 사용자가 찾는 조건에 맞는 글자가 s에 있는지 알려주자
     // 조건: 숫자를 찾아서 홀수인지
 
-    auto p = my_find_if(s.begin(), s.end(), [](int a) -> float { return 0.1f; });
+    auto p = s.begin();
+    while(p != s.end()) {
+        p = my_find_if(p+1, s.end(), [](char c) {
+            if(isdigit(c)) {
+                if(c & 1) {
+                    return true;
+                }
+            }
+            return false;
+        });
 
-    if(p != s.end()) {
-        cout << *p << "는 s의 " << distance(s.begin(), p) + 1 
-            << "번째 문자입니다." << endl;
+        if(p != s.end()) {
+            cout << *p << "는 s의 " << distance(s.begin(), p) + 1
+                << "번째 문자입니다." << endl;
+        }
+        else {
+            break;
+        }
     }
-    else {
-        cout << "s에 찾을것이 없다." << endl;
+
+    auto beg = s.begin();
+    while(true) {
+        auto p = my_find_if(beg, s.end(), [](char c) {
+            if(isdigit(c)) {
+                if(c & 1) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        if(p != s.end()) {
+            cout << *p << "는 s의 " << distance(s.begin(), p) + 1
+                << "번째 문자입니다." << endl;
+        }
+        else {
+            break;
+        }
+
+        beg = p + 1;
     }
 }
