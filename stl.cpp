2b9c10e40145bc6905ee36f4449114b6b890cfcb
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-14 15:43:37 +0900

    my_find

--- stl.cpp
+++ stl.cpp
@@ -1,7 +1,8 @@
 // --------------------------------------------------------------------------------
-// 2024 1학기 STL  월910화78        5월 13일 월요일                            (11주1)
+// 2024 1학기 STL  월910화78        5월 14일 화요일                           (11주2)
 // 
 // 알고리즘 함수는 반복자를 인자로 받는다.
+// 6월 11일 화요일(15주2) - 기말시험
 // --------------------------------------------------------------------------------
 
 #include <iostream>
@@ -16,7 +17,12 @@ extern bool 관찰;
 
 template <class Iter, class Val>
 Iter my_find(Iter first, Iter last, Val val) {
-
+    for(Iter it=first; it<last; ++it) {
+        if(*it == val) {
+            return it;
+        }
+    }
+    return last;
 }
 
 
@@ -34,7 +40,8 @@ int main() {
 
         auto p = my_find(s.begin(), s.end(), ch);
         if(p != s.end()) {
-            cout << "위치 - " << "을 찾음" << endl;
+            cout << ch << "는 s의 " << distance(s.begin(), p) + 1 
+                << "번째 문자입니다." << endl;
         }
         else {
             cout << ch << "는 s에 없는 글자입니다." << endl;
