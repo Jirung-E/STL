59ebd6c81a4f682ea72e600678020a3ef137dba2
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-21 16:21:07 +0900

    방법1 - set이용

--- stl.cpp
+++ stl.cpp
@@ -44,12 +44,18 @@ int main() {
         String word;
         cin >> word;
 
-        for(const String w : s) {
+        set<String> found;
+        for(const String& w : s) {
             auto p = search(w.begin(), w.end(), word.begin(), word.end());
             if(p != w.end()) {
-                cout << w << ' ';
+                found.insert(w);
             }
         }
+
+        for(const String& w : found) {
+            cout << w << ' ';
+        }
+        cout << endl;
     }
 
     //save("stl.cpp");
