020f9b3c1c6f524f47d5f12045ebf4298793198e
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-04 16:59:38 +0900

    ???

--- stl.cpp
+++ stl.cpp
@@ -17,6 +17,9 @@ extern bool 관찰;
 
 
 template <class T>
+concept 숫자만 = is_integral_v<T> | is_floating_point_v<T>;
+
+template <숫자만 T>
 T add(T a, T b) {
     return a + b;
 }
@@ -26,7 +29,7 @@ int main() {
     // [문제] 함수 템플릿 add를 만들어 주세요
     cout << add(1, 2) << endl;
     cout << add(1.1, 2.2) << endl;
-    cout << add("2024년"s, "6월 4일"s) << endl;
+    cout << add("2024년", "6월 4일") << endl;
 
     //save("stl.cpp");
 }
