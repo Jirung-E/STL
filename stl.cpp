e964c83f3da19202f0b02f3866903bc186ff8517
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-27 16:46:27 +0900

    정렬된 쌍으로 해결

--- stl.cpp
+++ stl.cpp
@@ -12,6 +12,13 @@ using namespace std;
 extern bool 관찰;
 
 
+struct PS : pair<string, string> {
+    PS(string s) : pair { s, s } {
+        sort(first.begin(), first.end());
+    }
+};
+
+
 int main() {
     ifstream in { "단어들.txt" };
     if(not in) {
@@ -19,104 +26,46 @@ int main() {
         return 0;
     }
 
-    vector<string> v;
-    v.reserve(100000);
+    vector<PS> v;
+    v.reserve(400000);
 
-    copy(istream_iterator<string> { in }, {}, back_inserter(v));
-    cout << "단어 개수: " << v.size() << endl;      // 369882개
-
-    cout << "정렬되어있나요? " << boolalpha << is_sorted(v.begin(), v.end()) << endl;      // true
-
-    auto p = unique(v.begin(), v.end());
-    if(p == v.end()) {
-        cout << "중복된 단어가 없습니다." << endl;        // .
+    string s;
+    while(in >> s) {
+        v.push_back(s);
     }
-    else {
-        cout << "중복된 단어가 있습니다." << endl;
-    }
+    cout << "단어 개수 - " << v.size() << endl;
 
-    // 길이가 가장 긴 단어
-    auto long_p = max_element(v.begin(), v.end(), [](const string& a, const string& b) {
-        return a.size() < b.size();
-    });
+    ranges::sort(v, less<>{}, &PS::first);
 
-    cout << "size: " << long_p->size() << endl;
-    cout << "longest word: " << *long_p << endl;
+    //cout << "어디서부터 출력할까요? ";
+    //int num;
+    //cin >> num;
 
-    // 원하는 길이의 단어 출력
-    cout << "원하는 단어 길이: ";
-    int word_len;
-    cin >> word_len;
-
-    for(const string& s : v 
-        | views::filter([](const string& s) { return s[0] == 'c'; })
-        | views::filter([word_len](const string& s) { return s.size() == word_len; })                    
-        | views::take(10)
-    ) {
-        cout << s << endl;
-    }
-
-    // 사용자가 입력한 단어가 사전에 있는지?
-    // 있다면 anagram출력
-
-    cout << endl; 
-
-    cout << "======================================" << endl;
-    cout << "        anagram 찾기 프로그램          " << endl;
-    cout << "======================================" << endl;
-
-    cout << endl;
+    //for(auto& [a, b] : v 
+    //    | views::drop(num)
+    //    | views::filter([](const auto& p) { return p.first.size() == 8; })
+    //    | views::take(100)
+    //) {
+    //    cout << a << " - " << b << endl;
+    //}
 
     while(true) {
         cout << "찾을 단어를 입력하세요: ";
-        string to_find;
-        cin >> to_find;
+        string s;
+        cin >> s;
 
-        if(not binary_search(v.begin(), v.end(), to_find)) {
-            cout << "사전에 없는 단어입니다." << endl << endl;
+        PS word { s };
+
+        auto range = ranges::equal_range(v, word.first, less<>{}, & PS::first);
+        if(range.empty()) {
+            cout << "찾을 수 없습니다." << endl;
             continue;
         }
 
-        cout << "가능한 조합의 개수: ";
-        long long int p_num = 1;
-        for(int i=0; i<to_find.size(); ++i) {
-            p_num *= i+1;
+        cout << s << "의 anagram들" << endl;
+        for(auto p : range) {
+            cout << p.second << " ";
         }
-        cout << p_num << endl << endl;
-
-        int dict[26] = {0, };
-        for(char ch : to_find) {
-            dict[ch - 'a']++;
-        }
-
-        for(const string& s : v) {
-            int d[26] = {0, };
-            for(char ch : s) {
-                d[ch - 'a']++;
-            }
-
-            bool is_anagram = true;
-            for(int i=0; i<26; ++i) {
-                if(dict[i] != d[i]) {
-                    is_anagram = false;
-                    break;
-                }
-            }
-
-            if(is_anagram) {
-                cout << s << " ";
-            }
-        }
-
-        //string word = to_find;
-        //sort(word.begin(), word.end());
-
-        //do {
-        //    if(binary_search(v.begin(), v.end(), word)) {
-        //        cout << word << " ";
-        //    }
-        //} while(next_permutation(word.begin(), word.end()));
-
         cout << endl;
         cout << endl;
     }
